34b7113f65204da3a283af97b80c09da92125107
Stefan Terdell <stefan.terdell+github@proton.me>
2025-06-19 18:39:39 +0000

    Symbol delimited functions

--- symbol-delimited-cases.ts
+++ symbol-delimited-cases.ts
@@ -35,16 +35,71 @@ type SymbolDelimitedCaseToWords<
     ? WordsAcc
     : [...WordsAcc, StringToLowerCase<Acc>];
 
-export type SnakeCaseToWords<S extends string> = SymbolDelimitedCaseToWords<
-  "_",
-  S
->;
-export type KebabCaseToWords<S extends string> = SymbolDelimitedCaseToWords<
-  "-",
-  S
->;
-export type UpperSnakeCaseToWords<S extends string> = SnakeCaseToWords<S>;
-export type UpperKebabCaseToWords<S extends string> = KebabCaseToWords<S>;
+function symbolDelimitedCaseToWords<
+  Delimiter extends string,
+  String extends string,
+>(
+  delimiter: Delimiter,
+  string: String,
+): SymbolDelimitedCaseToWords<Delimiter, String> {
+  const { words, word } = Array.from(string).reduce(
+    (acc: { words: string[]; word: string }, char) => {
+      if (char === delimiter) {
+        if (acc.word.length) {
+          acc.words.push(acc.word.toLocaleLowerCase());
+        }
+        acc.word = "";
+      } else {
+        acc.word += char;
+      }
+
+      return acc;
+    },
+    { words: [], word: "" },
+  );
+
+  if (word.length) {
+    words.push(word.toLocaleLowerCase());
+  }
+
+  return words as SymbolDelimitedCaseToWords<Delimiter, String>;
+}
+
+export type SnakeCaseToWords<String extends string> =
+  SymbolDelimitedCaseToWords<"_", String>;
+
+export function snakeCaseToWords<const String extends string>(
+  string: String,
+): SnakeCaseToWords<String> {
+  return symbolDelimitedCaseToWords("_", string);
+}
+
+export type KebabCaseToWords<String extends string> =
+  SymbolDelimitedCaseToWords<"-", String>;
+
+export function kebabCaseToWords<const String extends string>(
+  string: String,
+): KebabCaseToWords<String> {
+  return symbolDelimitedCaseToWords("-", string);
+}
+
+export type UpperSnakeCaseToWords<String extends string> =
+  SnakeCaseToWords<String>;
+
+export function upperSnakeCaseToWords<const String extends string>(
+  string: String,
+): UpperSnakeCaseToWords<String> {
+  return symbolDelimitedCaseToWords("_", string);
+}
+
+export type UpperKebabCaseToWords<String extends string> =
+  KebabCaseToWords<String>;
+
+export function upperKebabCaseToWords<const String extends string>(
+  string: String,
+): UpperKebabCaseToWords<String> {
+  return symbolDelimitedCaseToWords("-", string);
+}
 
 type WordsToSymbolDelimitedCase<
   Delimiter extends string,
@@ -58,16 +113,69 @@ type WordsToSymbolDelimitedCase<
     >
   : Acc;
 
+function wordsToSymbolDelimitedCase<
+  const Delimiter extends string,
+  const Words extends string[],
+>(
+  delimiter: Delimiter,
+  words: Words,
+  capitalize: true,
+): WordsToSymbolDelimitedCase<Delimiter, WordsToUpperCaseWords<Words>>;
+
+function wordsToSymbolDelimitedCase<
+  const Delimiter extends string,
+  const Words extends string[],
+>(
+  delimiter: Delimiter,
+  words: Words,
+  capitalize: false,
+): WordsToSymbolDelimitedCase<Delimiter, WordsToLowerCaseWords<Words>>;
+
+function wordsToSymbolDelimitedCase<
+  const Delimiter extends string,
+  const Words extends string[],
+>(delimiter: Delimiter, words: Words, capitalize: boolean) {
+  return (
+    capitalize
+      ? words.map((word) => word.toLocaleUpperCase())
+      : words.map((word) => word.toLocaleLowerCase())
+  ).join(delimiter);
+}
+
 export type WordsToKebabCase<W extends string[]> = WordsToSymbolDelimitedCase<
   "-",
   WordsToLowerCaseWords<W>
 >;
 
-export type WordsToUpperKebabCase<W extends string[]> =
-  WordsToSymbolDelimitedCase<"-", WordsToUpperCaseWords<W>>;
+export function wordsToKebabCase<const Words extends string[]>(
+  words: Words,
+): WordsToKebabCase<Words> {
+  return wordsToSymbolDelimitedCase("-", words, false);
+}
+
+export type WordsToUpperKebabCase<Words extends string[]> =
+  WordsToSymbolDelimitedCase<"-", WordsToUpperCaseWords<Words>>;
+
+export function wordsToUpperKebabCase<const Words extends string[]>(
+  words: Words,
+): WordsToUpperKebabCase<Words> {
+  return wordsToSymbolDelimitedCase("-", words, true);
+}
 
 export type WordsToSnakeCase<Words extends string[]> =
   WordsToSymbolDelimitedCase<"_", WordsToLowerCaseWords<Words>>;
 
+export function wordsToSnakeCase<const Words extends string[]>(
+  words: Words,
+): WordsToSnakeCase<Words> {
+  return wordsToSymbolDelimitedCase("_", words, false);
+}
+
 export type WordsToUpperSnakeCase<Words extends string[]> =
   WordsToSymbolDelimitedCase<"_", WordsToUpperCaseWords<Words>>;
+
+export function wordsToUpperSnakeCase<const Words extends string[]>(
+  words: Words,
+): WordsToUpperSnakeCase<Words> {
+  return wordsToSymbolDelimitedCase("_", words, true);
+}
--- symbol-delimited-cases_test.ts
+++ symbol-delimited-cases_test.ts
@@ -1,7 +1,20 @@
+import { assertEquals } from "@std/assert";
 import {
+  kebabCaseToWords,
+  KebabCaseToWords,
+  SnakeCaseToWords,
+  snakeCaseToWords,
+  upperKebabCaseToWords,
+  UpperKebabCaseToWords,
+  upperSnakeCaseToWords,
+  UpperSnakeCaseToWords,
+  wordsToKebabCase,
   WordsToKebabCase,
+  wordsToSnakeCase,
   WordsToSnakeCase,
+  wordsToUpperKebabCase,
   WordsToUpperKebabCase,
+  wordsToUpperSnakeCase,
   WordsToUpperSnakeCase,
 } from "./symbol-delimited-cases.ts";
 
@@ -9,14 +22,98 @@ import {
   ["din", "MAMMA", "en", "pApPa"]
 >;
 
+[
+  "din",
+  "mamma",
+  "en",
+  "pappa",
+] satisfies KebabCaseToWords<"din-mamma-en-pappa">;
+
 "DIN-MAMMA-EN-PAPPA" satisfies WordsToUpperKebabCase<
   ["din", "MAMMA", "en", "pApPa"]
 >;
+
+[
+  "din",
+  "mamma",
+  "en",
+  "pappa",
+] satisfies UpperKebabCaseToWords<"DIN-MAMMA-EN-PAPPA">;
 
 "din_mamma_en_pappa" satisfies WordsToSnakeCase<
   ["din", "MAMMA", "en", "pApPa"]
 >;
 
+[
+  "din",
+  "mamma",
+  "en",
+  "pappa",
+] satisfies SnakeCaseToWords<"din_mamma_en_pappa">;
+
 "DIN_MAMMA_EN_PAPPA" satisfies WordsToUpperSnakeCase<
   ["din", "MAMMA", "en", "pApPa"]
 >;
+
+[
+  "din",
+  "mamma",
+  "en",
+  "pappa",
+] satisfies UpperSnakeCaseToWords<"DIN_MAMMA_EN_PAPPA">;
+
+Deno.test(function snakeCaseToWordsShouldWork() {
+  const result = snakeCaseToWords("din_mamma_en_pappa");
+  const expected: typeof result = ["din", "mamma", "en", "pappa"];
+
+  assertEquals(result, expected);
+});
+
+Deno.test(function upperSnakeCaseToWordsShouldWork() {
+  const result = upperSnakeCaseToWords("DIN_MAMMA_EN_PAPPA");
+  const expected: typeof result = ["din", "mamma", "en", "pappa"];
+
+  assertEquals(result, expected);
+});
+
+Deno.test(function kebabCaseToWordsShouldWork() {
+  const result = kebabCaseToWords("din-mamma-en-pappa");
+  const expected: typeof result = ["din", "mamma", "en", "pappa"];
+
+  assertEquals(result, expected);
+});
+
+Deno.test(function upperKebabCaseToWordsShouldWork() {
+  const result = upperKebabCaseToWords("DIN-MAMMA-EN-PAPPA");
+  const expected: typeof result = ["din", "mamma", "en", "pappa"];
+
+  assertEquals(result, expected);
+});
+
+Deno.test(function wordsToSnakeCaseShouldWork() {
+  const result = wordsToSnakeCase(["din", "mamma", "en", "pappa"]);
+  const expected: typeof result = "din_mamma_en_pappa";
+
+  assertEquals(result, expected);
+});
+
+Deno.test(function wordsToUpperSnakeCaseShouldWork() {
+  const result = wordsToUpperSnakeCase(["din", "mamma", "en", "pappa"]);
+  const expected: typeof result = "DIN_MAMMA_EN_PAPPA";
+
+  assertEquals(result, expected);
+});
+
+Deno.test(function wordsToKebabCaseShouldWork() {
+  const result = wordsToKebabCase(["din", "mamma", "en", "pappa"]);
+  const expected: typeof result = "din-mamma-en-pappa";
+
+  assertEquals(result, expected);
+});
+
+Deno.test(function wordsToUpperKebabCaseShouldWork() {
+  const result = wordsToUpperKebabCase(["din", "mamma", "en", "pappa"]);
+  const expected: typeof result = "DIN-MAMMA-EN-PAPPA";
+
+  assertEquals(result, expected);
+});
