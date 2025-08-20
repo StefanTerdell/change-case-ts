2eccc751b37925134d76ea8899ce57dff73ed0b7
Stefan Terdell <stefan.terdell@cleura.com>
2025-08-20 12:04:49 +0200

    chmod +x .sh files and add .env generator

--- examples/env-file-and-zod/deno.sh
+++ examples/env-file-and-zod/deno.sh
@@ -1,1 +1,2 @@
 deno add npm:zod jsr:@std/env jsr:@stefan/change-case-ts
+echo "MY_ENV_VAR='Hello, world!'" >> .env
--- examples/env-file-and-zod/node.sh
+++ examples/env-file-and-zod/node.sh
@@ -1,2 +1,3 @@
 npm install --save-dev @types/node
 npm install dotenv zod change-case-ts
+echo "MY_ENV_VAR='Hello, world!'" >> .env
